e16a92c9921dceeb81dc3882379d9ec25dd9c80e
oshik <kihsogaming@gmail.com>
2025-08-16 02:43:37 +0545

    final 8162025243

--- backend/Ini_database/index.js
+++ backend/Ini_database/index.js
@@ -1,34 +1,32 @@
-// const mongoose = require("mongoose");
-// const VideoData = require("../models/video");
-// const mongoUrl =
-//   "mongodb+srv://kihsogaming:TMN5co49rNR4GWEs@cluster0.17qkru9.mongodb.net/Tourist?retryWrites=true&w=majority&appName=Cluster0";
+const mongoose = require("mongoose");
+const VideoData = require("../models/video");
+const mongoUrl =
+  "mongodb+srv://kihsogaming:TMN5co49rNR4GWEs@cluster0.17qkru9.mongodb.net/Tourist?retryWrites=true&w=majority&appName=Cluster0";
 
-// const initdb = async function () {
-//   const data = await VideoData.find();
-//   await Promise.all(
-//     data.map(async (doc) => {
-//       doc.video.owner = "Anonymous01";
-//       doc.video.url = doc.video.url.replace(
-//         "http://localhost:3030",
-//         "https://tourist-h76q.onrender.com"
-//       );
-//       doc.video.thumbnailUrl = doc.video.thumbnailUrl.replace(
-//         "http://localhost:3030",
-//         "https://tourist-h76q.onrender.com"
-//       );
-//       await doc.save(); // Save the full Mongoose document
-//     })
-//   );
-// };
-// // initdb();
+const initdb = async function () {
+  const data = await VideoData.find({ title: /undefined/ });
+  await Promise.all(
+    data.map(async (doc) => {
+      const cleanTitle = doc.title.replace(/undefined/, "");
+      const res = await VideoData.updateOne(
+        { _id: doc._id },
+        { $set: { title: cleanTitle } }
+      );
+    })
+  );
 
-// async function main() {
-//   await mongoose.connect(mongoUrl);
-// }
-// main()
-//   .then((res) => {
-//     console.log("server X database");
-//   })
-//   .catch((err) => {
-//     console.log("err happen connecting db");
-//   });
+  process.exit(0);
+};
+
+initdb();
+
+async function main() {
+  await mongoose.connect(mongoUrl);
+}
+main()
+  .then((res) => {
+    console.log("server X database");
+  })
+  .catch((err) => {
+    console.log("err happen connecting db");
+  });
